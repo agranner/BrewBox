e526bde5a2437c1a010b52eb41c03d1deffaa720
Aubrey Granner <agranner@saygotit.com>
2015-02-07 12:39:12 -0800

    states
    from sousviduino

--- BrewBox.ino
+++ BrewBox.ino
@@ -4,3 +4,44 @@
 */
 
 
+int LEDpin = 13;
+int incomingByte = 0;	// for incoming serial data
+
+void setup()
+{
+	Serial.begin(9600)
+	pinMode(LEDpin, OUTPUT)
+}
+
+
+void loop()
+{
+   // wait for button release before changing state
+   while(ReadButtons() != 0) {}
+ 
+   lcd.clear();
+ 
+   Serial.println(opState);
+ 
+   switch (opState)
+   {
+   case OFF:
+      Off();
+      break;
+   case SETP:
+      Tune_Sp();
+      break;
+    case RUN:
+      Run();
+      break;
+   case TUNE_P:
+      TuneP();
+      break;
+   case TUNE_I:
+      TuneI();
+      break;
+   case TUNE_D:
+      TuneD();
+      break;
+   }
+}
